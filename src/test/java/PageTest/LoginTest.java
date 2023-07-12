349aafd88053a7cabb26f79669bb0357c1ffafe5
Administrator <Administrator@172.31.30.139>
2023-07-12 08:35:04 +0000

    changed logintest page

--- src/test/java/PageTest/LoginTest.java
+++ src/test/java/PageTest/LoginTest.java
@@ -4,7 +4,11 @@ public class LoginTest {
 
 	public static void main(String[] args) {
 		
-
+	System.out.println("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");
+	
+	System.out.println("https://github.com/shivavissu/SampleProject1.git");
+	System.out.println("https://github.com/shivavissu/webDriverSamplesB34.git");
+	
 	}
 
 }
